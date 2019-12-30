7e2d0093aa7cd652e8d1cab8ddfc623a5b52346d
temirlan <bolotbekov2288@gmail.com>
2019-12-30 09:24:09 +0600

    tratata

---
+++ task13.py
@@ -0,0 +1,9 @@
+# Для заданного целого числа N выведите все квадраты целых чисел, где
+# квадрат меньше или равен N в порядке возрастания.
+
+# x = int(input('Vvedite chislo :'))
+# print([c**2 for c in range(1,x+1) if c**2 <=x])
+
+def func(arg1,arg2 ='1'):
+    print(arg1,arg2)
+func(arg1='1',arg2='2')
